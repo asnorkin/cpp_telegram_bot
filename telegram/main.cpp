a28042301dea34c971a5e075f7a6dded1ac81b3c
Alexander <snorkin@phystech.edu>
2018-12-27 11:54:14 +0300

    Update main.cpp

--- telegram/main.cpp
+++ telegram/main.cpp
@@ -1,10 +1,27 @@
 #include "bot.h"
 
-constexpr auto kBotToken = "489689257:AAE35Lj-cOSGpyqq8szl1J6A5VkGY6Arb54";
-constexpr auto kBotFirstName = "blablabot";
+#include <ifstream>
+#include <string>
 
+std::string ReadToken(const std::string& tokenFile) {
+    std::string token;
+    std::ifstream inpFile(tokenFile);
+    inpFile >> token;
+    inpFile.close();
+    return token;
+}
+
+std::string ReadName(const std::string& nameFile) {
+    std::string name;
+    std::ifstream inpFile(nameFile);
+    inpFile >> name;
+    inpFile.close();
+    return name;
+}
 
 int main(int argc, char* argv[]) {
+    const auto& kBotToken = ReadToken("token.txt");
+    const auto& kBotFirstName = ReadName("name.txt");
     Bot bot(kBotToken, kBotFirstName, "information");
     bot.Run();
     return 0;
